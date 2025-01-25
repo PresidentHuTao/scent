e1ca135582a3a279b09079838dc03aa6557757f4
maithaongoc111103@gmail.com <Thaongoc03@>
2025-01-25 16:00:07 +0700

    sửa SanPhamrespone

--- src/main/java/com/example/scent/respone/SanPhamRespone.java
+++ src/main/java/com/example/scent/respone/SanPhamRespone.java
@@ -5,10 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class SanPhamRespone {
 
     private Integer id;
@@ -20,4 +17,55 @@ public class SanPhamRespone {
     private String thuongHieu;
 
     private String danhMuc;
+
+    public SanPhamRespone(Integer id, String ten, String moTa, String thuongHieu, String danhMuc) {
+        this.id = id;
+        this.ten = ten;
+        this.moTa = moTa;
+        this.thuongHieu = thuongHieu;
+        this.danhMuc = danhMuc;
+    }
+
+    public SanPhamRespone() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public String getMoTa() {
+        return moTa;
+    }
+
+    public String getThuongHieu() {
+        return thuongHieu;
+    }
+
+    public String getDanhMuc() {
+        return danhMuc;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public void setMoTa(String moTa) {
+        this.moTa = moTa;
+    }
+
+    public void setThuongHieu(String thuongHieu) {
+        this.thuongHieu = thuongHieu;
+    }
+
+    public void setDanhMuc(String danhMuc) {
+        this.danhMuc = danhMuc;
+    }
 }
